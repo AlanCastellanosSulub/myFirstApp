a80dff12985810e09cff16df564e383becd1af6a
AlanCastellanosSulub <adjcastellanos@gmail.com>
2022-10-03 10:15:56 -0500

    Third Commit Homework

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $viewData = [];
+    $viewData["title"] = "Home Page - Online Store";
+    return view('home.index')->with("viewData", $viewData);
 });
 
-Route::get('/index', function () {
-    return view('index');
-});
-
+Route::get('/about','App\Http\Controllers\HomeController@about')->name("home.about");
